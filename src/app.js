1be0ce8d66b1ad5decb633b0a878e71a128cd2a9
Pedro Pereira <plp.leoncio@gmail.com>
2022-09-05 13:34:36 -0300

    implementação do DayJS

--- src/app.js
+++ src/app.js
@@ -1,56 +1,66 @@
 import express from 'express';
 import cors from "cors";
 import { MongoClient } from 'mongodb';
+import joi from 'joi';
 import dotenv from 'dotenv'
+import dayjs from 'dayjs'
+
+
 dotenv.config()
 
 const server = express();
 server.use(express.json())
 server.use(cors());
 
-const mongoClient = new MongoClient(process.env.MONGO_URI)
 let db
+const mongoClient = new MongoClient(process.env.MONGO_URI)
 mongoClient.connect(() => {
     db = mongoClient.db('api-uol')
 })
 
-
-let participantes = [
-    {
-    name: "a", 
-    lastStatus: 12313123
-    }
-]
-
-
-let historico = [
-    /*
-    {
-    from: 'João',
-    to: 'Todos',
-    text: 'oi galera',
-    type: 'message',
-    time: '20:04:37'
-    } */
-    1,2,3,4,5,6,7,8
-]
-server.get('/teste', function (req, res){
-    console.log(db)
-    res.send("ok")
+const participantesSchema = joi.object({
+    name: joi.string().required(),
+})
+const messagesSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required()  
 })
 
+server.get('/teste', async function (req, res) {
+    
+    try {
+        const data = dayjs().locale('pt-br').format('HH:mm:ss')
+        res.send(await db.collection('messages').find().toArray())
+    } catch (error) {
+        res.send("deu nao")
+    }
+})
 server.post('/participants', async function (req, res){   
-    if (req.body.name === "") return res.status(422).send({erro: "nome invalido"})
 
-    if (!(participantes.find(nomes => nomes.name === req.body.name))){
+    const validation = participantesSchema.validate(req.body)
+    if (validation.error){
+        return res.sendStatus(422)
+    }
 
-        const response = await db.collection('participantes').insertOne({
-            name: req.body.name
-        })
-        res.status(201).send({message: "participante inserido com sucesso", id: response.insertedId})       
-        
-    } else return res.status(409).send({erro: "nome ja cadastrado"})
-       
+    const arrayParticipantes = await db.collection('participantes').find().toArray()
+    if(arrayParticipantes.find(e => e.name === req.body.name)) return res.sendStatus(409)
+
+    const response = await db.collection('participantes').insertOne({
+        name: req.body.name,
+        lastStatus: Date.now()
+    })
+
+    await db.collection('messages').insertOne({
+        from: req.body.name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs().locale('pt-br').format('HH:mm:ss'),
+    })
+
+    res.status(201).send({message: "participante inserido com sucesso", id: response.insertedId})
+  
 })
 
 server.get('/participants', async function (req, res){
@@ -66,13 +76,22 @@ server.get('/participants', async function (req, res){
 })
 
 server.post('/messages', function (req, res){
+
+    const validation = messagesSchema.validate(req.body, {abortEarly: false})
+    if(validation.error){
+        const erros = validation.error.details.map(e => e.message)
+        res.status(422).send(erros)
+        return
+    }
+   
+
     const {to, text, type} = req.body
     const {user} = req.headers
 
-    if (to === "" || text === "") return res.status(422).send({erro: "destinatario ou mensagem invalida"})
-    if (!(participantes.find(nomes => nomes.name === user))) return res.status(422).send({erro: "usuario invalido"})
+    //if (to === "" || text === "") return res.status(422).send({erro: "destinatario ou mensagem invalida"})
+    //if (!(participantes.find(nomes => nomes.name === user))) return res.status(422).send({erro: "usuario invalido"})
 
-    if (type === "message" || type === "private_message"){
+    //if (type === "message" || type === "private_message"){
 
         historico.push({
             from: user,
@@ -83,7 +102,7 @@ server.post('/messages', function (req, res){
         })
 
         return res.sendStatus(201)
-    } else return res.status(422).send({erro: "tipo de mensagem invalida"})
+    //} else return res.status(422).send({erro: "tipo de mensagem invalida"})
 })
 
 server.get('/messages', function (req, res){
